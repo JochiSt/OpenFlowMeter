cadf3856a9a6b9adb9fc61f15a19a77bab0b6094
JochiSt <jochen.steinmann@web.de>
2022-02-07 17:37:58 +0100

    increase sampling time to 55.5 cycles

--- firmware/Src/adc.c
+++ firmware/Src/adc.c
@@ -50,7 +50,7 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_7CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
