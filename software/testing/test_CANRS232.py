b597f12c2a8df5627bbaf2a7cb30f3aaf5efd9fd
JochiSt <jochen.steinmann@web.de>
2022-02-09 10:34:23 +0100

    changed to terminate able structure
    and close usbtin after exiting main loop.

--- software/testing/test_CANRS232.py
+++ software/testing/test_CANRS232.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  9 08:42:40 2022
-
-@author: steinmann
-"""
 
 import sys
 sys.path.append("../pyUSBtin")
@@ -13,14 +8,34 @@ from pyusbtin.canmessage import CANMessage
 from time import sleep
 
 def log_data(msg):
-    print(msg)
+    print("Message ID %x"%(msg.mid) )
+    print("length %d"%(msg.dlc) )
+    if msg.mid == 0x123:
+        if msg.dlc == 8:
+            print(msg)
 
 usbtin = USBtin()
 usbtin.connect("COM7")
 usbtin.add_message_listener(log_data)
 usbtin.open_can_channel(500000, USBtin.ACTIVE)
 
-while(True):
-    print("tick")
-    #usbtin.send(CANMessage(0x100, "\x11"))
-    sleep(1)
+def main():
+    try:
+        while True:
+            print("tick")
+
+            #usbtin.send(CANMessage(0x123, [0, 0]))
+
+            sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+    # shutting down CAN interface
+    usbtin.stop_rx_thread()
+    usbtin.close_can_channel()
+    # disconnect from USBtin
+    usbtin.disconnect()
+
+if __name__ == "__main__":
+    main()
+
