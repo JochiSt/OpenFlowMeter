93704230099761e8af19f59d9074847291ec0fca
JochiSt <jochen.steinmann@web.de>
2022-02-12 08:52:29 +0100

    enhanced measurement
    doing some more points (changed to np.linspace)
    added current conversion

--- software/testing/sweep_DAC.py
+++ software/testing/sweep_DAC.py
@@ -26,7 +26,7 @@ def main():
     channels = [0,1]    # channels, we want to analyse
 
     try:
-        dac, voltage, current = DACsweep(ofm=ofm, channel=channels, steps = 256)
+        dac, voltage, current = DACsweep(ofm=ofm, channel=channels, steps = 16)
         measurement_successful = True
     except Exception as e:
         print(e)
@@ -40,16 +40,23 @@ def main():
     # convert from ADC LSB into SI
     for chan in channels:
         voltage[chan] = 3.3/4095 * voltage[chan]
-        current[chan] = 3.3/4095 * current[chan]
+        current[chan] = 3.3/4095 * current[chan] * 10e-3    # from simulation 1V = 10mA
 
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
     # analyse data
     for chan in channels:
-        plt.plot(dac[chan], voltage[chan], label="U%d"%chan)
-        plt.plot(dac[chan], current[chan], label="I%d"%chan)
+        ax1.plot(dac[chan], voltage[chan],          label="U%d"%chan)
+        ax2.plot(dac[chan], current[chan]*1000, label="I%d"%chan)
 
-    plt.ylabel('ADC / V')
-    plt.xlabel('DAC / LSB')
-    plt.legend()
+    ax1.set_xlabel('DAC / LSB')
+    ax1.set_ylabel('voltage / V')
+    ax2.set_ylabel("current / mA")
+
+    lines, labels    = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax2.legend(lines + lines2, labels + labels2, loc=0)
+
     plt.show()
 
     resistance = {}
@@ -75,7 +82,7 @@ def DACsweep(ofm, channel = [0, 1], steps = 128):
         current[chan] = np.array([])
         dac[chan] = np.array([])
 
-    for DAC in range(0, 1024, steps):
+    for DAC in np.linspace(start=0, stop=1023, num=steps, endpoint=True).astype(int):
         # set DAC
         DACset = [0, 0]
         for chan in channel:
@@ -85,8 +92,8 @@ def DACsweep(ofm, channel = [0, 1], steps = 128):
         if ofm:
             ofm.setDAC(DACset[0], DACset[1])
 
-        # wait 5 seconds to settle everything
-        time.sleep(5)
+        # wait some seconds to settle everything
+        time.sleep(2)
 
         # wait until we have a new message
         if ofm:
