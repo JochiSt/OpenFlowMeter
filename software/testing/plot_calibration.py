f08f5e492c17dd5fe7cc7c6ff4028fa328fe4332
JochiSt <jochen.steinmann@web.de>
2022-03-08 19:37:24 +0100

    added plot of MMcurrent vs DAC settings

--- software/testing/plot_calibration.py
+++ software/testing/plot_calibration.py
@@ -16,6 +16,14 @@ def plot_calibration(filename):
     voltage = npzfile['voltage']
     current = npzfile['current']            # OpenFlowMeter measured current
     MMcurrent = npzfile['MMcurrent']  # Multimeter measured current
+    dac_setp = npzfile['dac_steps']
+
+    # we have to recover the DAC setting for each measurement point
+    # only all DAC settings are given
+    repetitions = int( len(voltage)/len(dac_setp) )
+    dac_steps = np.array([])
+    for dac in dac_setp:
+        dac_steps = np.append(dac_steps, np.ones(repetitions)*dac)
 
     MMcurrent =np.array(  [0 if v is None else v for v in MMcurrent] )
 
@@ -24,25 +32,38 @@ def plot_calibration(filename):
     MMcurrent /= 10 # to get it in mA
 
     fig, ax1 = plt.subplots()
-
+    ax1.set_title("OFM current vs. Multimeter current")
     color = 'tab:red'
     ax1.set_xlabel("multimeter current / mA")
     ax1.set_ylabel("OFM current / mA", color=color)
     ax1.plot( MMcurrent, current, color=color, label="calibration", marker="." )
-    ax1.plot( [0,30], [0,30], color=color, linestyle='--',label="y=x")
+    ax1.plot( [0,33], [0,33], color=color, linestyle='--',label="y=x")
+    ax1.set_ylim([0,33])
+    ax1.set_xlim([0,max(MMcurrent)])
     ax1.tick_params(axis='y', labelcolor=color)
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
     color = 'tab:blue'
     ax2.set_ylabel('OFM voltage / V', color=color)  # we already handled the x-label with ax1
     ax2.plot( MMcurrent, voltage, color=color)
+    ax2.plot( [0,40], [0,4], color=color, linestyle='--',label="y=x")
     ax2.tick_params(axis='y', labelcolor=color)
+    ax2.set_ylim([0,4.0])
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
 
-    #return npzfile['dac'], npzfile['current'], npzfile['voltage']
+    fig, ax1 = plt.subplots()
+    ax1.set_title("Multimeter current vs. DAC setpoint")
+    color = 'tab:red'
+    ax1.set_xlabel("DAC setting / LSB")
+    ax1.set_ylabel("multimeter current / mA", color=color)
+    ax1.plot( dac_steps, MMcurrent, color=color, label="calibration", marker="." )
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.set_ylim([0,max(MMcurrent)])
+    ax1.set_xlim([0,max(dac_steps)])
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.show()
 
 if __name__ == "__main__":
     #plot_calibration("calibration_20220307_133034_CH0_10.npz")
