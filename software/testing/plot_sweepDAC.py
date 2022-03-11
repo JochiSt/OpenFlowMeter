023b3a804cbe9bff9ec3eacf3fec0829b18a5d0b
JochiSt <jochen.steinmann@web.de>
2022-03-11 12:56:54 +0100

    added a bit more analysis and storage of plots to disk

--- software/testing/plot_sweepDAC.py
+++ software/testing/plot_sweepDAC.py
@@ -4,12 +4,13 @@
 """
 
 import numpy as np
-from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 import sys
 sys.path.append("../")
 
-import matplotlib.pyplot as plt
+import os
+
 
 def plot_sweepDAC(filename):
     npzfile = np.load(filename)
@@ -40,7 +41,10 @@ def plot_sweepDAC(filename):
     color = 'tab:green'
     ax3.spines['right'].set_position(('outward', 60))
     ax3.set_ylabel('OFM resistance / Ohm', color=color)  # we already handled the x-label with ax1
-    ax3.plot( dac, voltage/current, color=color)
+
+    resistance = voltage / current
+
+    ax3.plot( dac[3:], resistance[3:], color=color)
     ax3.tick_params(axis='y', labelcolor=color)
     ax3.set_ylim([100*0.9,100*1.1])
 
@@ -54,14 +58,20 @@ def plot_sweepDAC(filename):
         return idx
 
     dac_1mA_idx = find_nearest_idx( dac, 32)
-    resistance_1mA = voltage[dac_1mA_idx] / current[dac_1mA_idx]
+    resistance_1mA = resistance[dac_1mA_idx]
     print("resistance at DAC=",dac[dac_1mA_idx],"LSBs is ", resistance_1mA , " Ohm" )
+
+    print( resistance[dac_1mA_idx-3 : dac_1mA_idx+3] )
 
     ax3.hlines(resistance_1mA, 0, 1023, color=color, linestyle="--")
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.savefig(os.path.splitext(filename)[0]+".png")
     plt.show()
 
 if __name__ == "__main__":
     plot_sweepDAC("measurement_20220309_065823_CH0_1023.npz")
-    plot_sweepDAC("measurement_20220309_065823_CH1_1023.npz")
+    #plot_sweepDAC("measurement_20220309_065823_CH1_1023.npz")
+
+    plot_sweepDAC("measurement_20220311_081058_CH0_1023.npz")
+    #plot_sweepDAC("measurement_20220311_081058_CH1_1023.npz")
