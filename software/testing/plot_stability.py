e7d226388a74f185df7a4816519e879efb798e50
JochiSt <jochen.steinmann@web.de>
2022-03-11 14:34:00 +0100

    tweakened plots to be a bit smaller
    since these are "just" control plots, I shrinked their size.

--- software/testing/plot_stability.py
+++ software/testing/plot_stability.py
@@ -14,6 +14,8 @@ def plot_stability(filename):
     npzfile = np.load(filename)
     dac_steps = npzfile['dac_steps']
 
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
     # plot all points
     all_U = np.array([])
     all_I = np.array([])
@@ -23,64 +25,64 @@ def plot_stability(filename):
         all_U = np.append( all_U, npzfile['U'+str(dac)])
         all_I = np.append( all_I, npzfile['I'+str(dac)])
 
-    plt.xlabel("voltage / LSB")
-    plt.ylabel("current / LSB")
-    plt.plot( all_U, all_I,   label="all measurement points", marker=".", linestyle = "")
-    plt.legend()
-    plt.show()
+    ax1.set_xlabel("voltage / LSB")
+    ax1.set_ylabel("current / LSB")
+    ax1.plot( all_U, all_I,   label="all points", marker=".", linestyle = "")
+    ax1.legend()
 
-    fig, ax1 = plt.subplots()
-    ax1.set_title("OFM stability")
+    ax2.set_title("OFM stability")
     # plot voltage
     for dac in dac_steps:
         if dac == 0:
             continue
-        ax1.plot( npzfile['U'+str(dac)], label="DAC = %d"%(dac) )
+        ax2.plot( npzfile['U'+str(dac)], label="DAC = %d"%(dac) )
 
-    ax1.set_ylabel("voltage / LSB")
-    ax1.set_xlabel("measurement point")
-    ax1.legend()
+    ax2.set_ylabel("voltage / LSB")
+    ax2.set_xlabel("measurement point")
+    ax2.legend()
 
-    ax2 = ax1.twinx()
+    ax2a = ax2.twinx()
 
     # plot current
     for dac in dac_steps:
         if dac == 0:
             continue
-        ax2.plot( npzfile['I'+str(dac)], label="DAC = %d"%(dac), linestyle="--" )
+        ax2a.plot( npzfile['I'+str(dac)], label="DAC = %d"%(dac), linestyle="--" )
 
-    ax2.set_ylabel("current / LSB (dotted)")
-    ax2.set_ylim( [min(all_I)*0.8, max(all_I)*1.2] )
+    ax2a.set_ylabel("current / LSB (dotted)")
+    ax2a.set_ylim( [min(all_I)*0.8, max(all_I)*1.2] )
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
 
     ###
     # histogramming
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
     for dac in dac_steps:
         if dac == 0:
             continue
         current = np.array(npzfile['I'+str(dac)])
-        n, bins, patches = plt.hist(current , np.linspace(0,1024,512),  label="DAC = %d"%(dac))
+        n, bins, patches = ax1.hist(current , np.linspace(0,1024,128),  label="DAC = %d"%(dac))
 
-    plt.xlabel('OFM measured current / LSB')
-    plt.ylabel('#')
+    ax1.set_xlabel('OFM measured current / LSB')
+    ax1.set_ylabel('#')
    # plt.yscale('log', nonposy='clip')
-    plt.title('Histogram of OFM measured current')
-    plt.legend()
-    plt.show()
+    ax1.set_title('Current')
+    ax1.legend()
 
     for dac in dac_steps:
         if dac == 0:
             continue
         voltage = np.array(npzfile['U'+str(dac)])
-        n, bins, patches = plt.hist(voltage , np.linspace(0,1024,512),  label="DAC = %d"%(dac))
+        n, bins, patches = ax2.hist(voltage , np.linspace(0,1024,128),  label="DAC = %d"%(dac))
 
-    plt.xlabel('OFM measured voltage / LSB')
-    plt.ylabel('#')
+    ax2.set_xlabel('OFM measured voltage / LSB')
+    ax2.set_ylabel('#')
     #plt.yscale('log', nonposy='clip')
-    plt.title('Histogram of OFM measured voltage')
-    plt.legend()
+    ax2.set_title('Voltage')
+    ax2.legend()
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
 
 
@@ -97,10 +99,6 @@ def plot_stability(filename):
     plt.show()
 
 if __name__ == "__main__":
-    #plot_stability("stability_20220220_211134_CH0_1000.npz")
-    #plot_stability("stability_20220220_211134_CH1_1000.npz")
-
-    #plot_stability("stability_20220311_110956_CH0_1000.npz")
-    #plot_stability("stability_20220311_115830_CH0_1000.npz")
-
+    #plot_stability("stability_20220311_124812_CH0_100.npz")
     plot_stability("stability_20220311_124812_CH1_100.npz")
+
