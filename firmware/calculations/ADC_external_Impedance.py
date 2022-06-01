19fa4b1a8df62c4d70d04358e66fbc45c29e5083
JochiSt <jochen.steinmann@web.de>
2022-06-01 10:03:24 +0200

    added all examples from datasheet
    and improved the output formatting.

--- firmware/calculations/ADC_external_Impedance.py
+++ firmware/calculations/ADC_external_Impedance.py
@@ -8,19 +8,27 @@ def Rext(TS, Fadc, Nbits=12):
         see page 75 of https://www.st.com/resource/en/datasheet/stm32f103c8.pdf for details
     """
 
-    print("TS = %.1f cycles\tFadc=%.2f MHz"%(TS, Fadc/1e6))
+    print("TS = %4.1f cycles\tFadc=%6.2f MHz"%(TS, Fadc/1e6))
     Cadc = 8e-12
     Radc = 1e3
     RAIN = TS / (Fadc * Cadc * math.log (math.pow(2, Nbits+2)) ) - Radc
     return RAIN / 1000
 
+print("#"*80)
 print("Calculate the maximal allowed external impedance:")
-
-TS = 55.5
-Fadc = 6.75e6
-
+print("\n")
+print("Setup used in OpenFlowMeter")
 
 print("%8.2f kOhm"%(Rext(55.5, 6.75e6)) )
 
+print("\n")
+print("#"*80)
+print("Examples from datasheet:")
 #example from datasheet
+print("%8.2f kOhm"%(Rext( 1.5, 14e6)) )
+print("%8.2f kOhm"%(Rext( 7.5, 14e6)) )
+print("%8.2f kOhm"%(Rext(13.5, 14e6)) )
+print("%8.2f kOhm"%(Rext(28.5, 14e6)) )
+print("%8.2f kOhm"%(Rext(41.5, 14e6)) )
 print("%8.2f kOhm"%(Rext(55.5, 14e6)) )
+print("%8.2f kOhm"%(Rext(71.5, 14e6)) )
