07f1befaa3c2cf06d09b89ea928d92f905a039fa
JochiSt <jochen.steinmann@web.de>
2022-06-01 09:58:51 +0200

    moved calculation into function

--- firmware/calculations/ADC_external_Impedance.py
+++ firmware/calculations/ADC_external_Impedance.py
@@ -2,18 +2,25 @@
 
 import math
 
+def Rext(TS, Fadc, Nbits=12):
+    """
+        returns the maximal allowed external impedance in kOhm
+        see page 75 of https://www.st.com/resource/en/datasheet/stm32f103c8.pdf for details
+    """
+
+    print("TS = %.1f cycles\tFadc=%.2f MHz"%(TS, Fadc/1e6))
+    Cadc = 8e-12
+    Radc = 1e3
+    RAIN = TS / (Fadc * Cadc * math.log (math.pow(2, Nbits+2)) ) - Radc
+    return RAIN / 1000
+
+print("Calculate the maximal allowed external impedance:")
+
 TS = 55.5
 Fadc = 6.75e6
-Cadc = 8e-12
-Nbits = 12
-Radc = 1e3
 
-RAIN = TS / (Fadc * Cadc * math.log (math.pow(2, Nbits+2)) ) - Radc
-print("%8.2f kOhm"%(RAIN/1000) )
+
+print("%8.2f kOhm"%(Rext(55.5, 6.75e6)) )
 
 #example from datasheet
-TS = 55.5
-Fadc = 14e6
-
-RAIN = TS / (Fadc * Cadc * math.log (math.pow(2, Nbits+2)) ) - Radc
-print("%8.2f kOhm"%(RAIN/1000) )
+print("%8.2f kOhm"%(Rext(55.5, 14e6)) )
