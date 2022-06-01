0a4e4ddacd3c46943615ec7472c01731e18ac2f7
JochiSt <jochen.steinmann@web.de>
2022-06-01 10:05:32 +0200

    cosmetic changes to the output

--- firmware/calculations/ADC_external_Impedance.py
+++ firmware/calculations/ADC_external_Impedance.py
@@ -8,7 +8,7 @@ def Rext(TS, Fadc, Nbits=12):
         see page 75 of https://www.st.com/resource/en/datasheet/stm32f103c8.pdf for details
     """
 
-    print("TS = %4.1f cycles\tFadc=%6.2f MHz"%(TS, Fadc/1e6))
+    print("TS = %4.1f cycles\tFadc = %6.2f MHz"%(TS, Fadc/1e6))
     Cadc = 8e-12
     Radc = 1e3
     RAIN = TS / (Fadc * Cadc * math.log (math.pow(2, Nbits+2)) ) - Radc
