bfa86a685b46dfe3a47f5dd9203f9a9e941fed5b
JochiSt <jochen.steinmann@web.de>
2022-11-02 10:27:48 +0100

    do not run the ADC in continuous mode
    We have to toggle the GAIN pins between two measurements

--- firmware/Src/adc.c
+++ firmware/Src/adc.c
@@ -37,7 +37,7 @@ void MX_ADC1_Init(void)
   */
   hadc1.Instance = ADC1;
   hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
-  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
