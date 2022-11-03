d9990e865f0102b7965d639fa26ef3106cd25971
JochiSt <jochen.steinmann@web.de>
2022-11-03 09:37:49 +0100

    added function to easily set the GAIN selection

--- firmware/Inc/utils.h
+++ firmware/Inc/utils.h
@@ -10,6 +10,10 @@
 // green    -> TIM2
 #define LED_STATUS(Op) HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_##Op)
 
+// GAIN_I & GAIN_U
+#define GAIN_I(Op) HAL_GPIO_WritePin(SEL_GAIN_I_GPIO_Port, SEL_GAIN_I_Pin, GPIO_PIN_##Op)
+#define GAIN_U(Op) HAL_GPIO_WritePin(SEL_GAIN_U_GPIO_Port, SEL_GAIN_U_Pin, GPIO_PIN_##Op)
+
 #define LED_ERROR_TOGGLE	HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
 #define LED_CANRX_TOGGLE	HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
 #define LED_CANTX_TOGGLE	HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
