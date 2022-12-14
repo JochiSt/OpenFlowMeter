c59129bdb8085ba05c346f405ad11d8b6449b756
JochiSt <jochen.steinmann@web.de>
2022-12-14 09:38:09 +0100

    fixed typo in copy and paste

--- firmware/Inc/i2c_utils.h
+++ firmware/Inc/i2c_utils.h
@@ -1,6 +1,6 @@
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __I2C_SCANNER_H__
-#define __I2C_SCANNER_H__
+#ifndef __I2C_UTILS_H__
+#define __I2C_UTILS_H__
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,6 +20,8 @@ extern "C" {
 
 
 /* USER CODE BEGIN Prototypes */
+void i2c_init_TMP100(I2C_HandleTypeDef* i2cHandle, uint8_t TMP100_addr);
+uint16_t i2c_read_TMP100(I2C_HandleTypeDef* i2cHandle, uint8_t TMP100_addr);
 
 /* USER CODE END Prototypes */
 
@@ -27,6 +29,6 @@ extern "C" {
 }
 #endif
 
-#endif /* __I2C_SCANNER_H__ */
+#endif /* __I2C_UTILS_H__ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
