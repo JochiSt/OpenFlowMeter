fb6c5c0995a848c0338ad57afaaac40d84e6d668
JochiSt <jochen.steinmann@web.de>
2022-12-15 18:35:07 +0100

    implemented EEPROM read and write
    not tested!

--- firmware/Src/i2c_utils.c
+++ firmware/Src/i2c_utils.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 #include "i2c_utils.h"
+#include "utils.h"
 
 /**
  *
  */
-uint8_t write16_EEPROM(I2C_HandleTypeDef* i2cHandle, uint8_t i2c_addr, uint8_t addr, uint16_t value){
+uint8_t write16_EEPROM(I2C_HandleTypeDef* i2cHandle, uint8_t EEPROM_addr, uint8_t addr, uint16_t value){
   // store a 16 bit number in the EEPROM
-
-  return 0x00;
+  uint8_t buf[3] = {0x00};
+  buf[0] = addr;
+  buf[1] = upper(value);
+  buf[2] = lower(value);
+  HAL_StatusTypeDef result;
+  result = HAL_I2C_Master_Transmit(i2cHandle, EEPROM_addr<<1, buf, 3, HAL_MAX_DELAY);
+  if ( result != HAL_OK ) {
+	printf("unable to set address of EEPROM\r\n");
+	return 1;
+  }
+  return 0;
 }
 
 /**
  *
  */
-uint8_t read16_EEPROM(I2C_HandleTypeDef* i2cHandle, uint8_t i2c_addr, uint8_t addr){
+uint8_t read16_EEPROM(I2C_HandleTypeDef* i2cHandle, uint8_t EEPROM_addr, uint8_t addr){
   // read a 16 bit number from the EEPROM
 
-  return 0x00;
+  uint8_t buf[2] = {0x00};
+  buf[0] = addr;
+  HAL_StatusTypeDef result;
+  result = HAL_I2C_Master_Transmit(i2cHandle, EEPROM_addr<<1, buf, 1, HAL_MAX_DELAY);
+  if ( result != HAL_OK ) {
+	printf("unable to set address of EEPROM\r\n");
+	return 0xFF;
+  }
+  result = HAL_I2C_Master_Receive(i2cHandle, EEPROM_addr<<1, buf, 2, HAL_MAX_DELAY);
+  if ( result != HAL_OK ) {
+    printf("Error TMP100 RX\r\n");
+    return 0xFF;
+  }
+  return buf[0] << 8 | buf[1];
 }
 
 
