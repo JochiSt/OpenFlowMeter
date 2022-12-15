d79c591e48837d7d1ba59478c9f56a4c784228b0
JochiSt <jochen.steinmann@web.de>
2022-12-15 09:55:58 +0100

    reduced buffer size for reading TMP100

--- firmware/Src/i2c_utils.c
+++ firmware/Src/i2c_utils.c
@@ -52,12 +52,12 @@ void i2c_init_TMP100(I2C_HandleTypeDef* i2cHandle, uint8_t TMP100_addr){
 uint16_t i2c_read_TMP100(I2C_HandleTypeDef* i2cHandle, uint8_t TMP100_addr){
   printf("Reading TMP100\r\n");
 
-  uint8_t buf[12];  // buffer for sending and receiving I2C data
+  uint8_t buf[2];  // buffer for sending and receiving I2C data
   int16_t val = 0xFFFF;
   HAL_StatusTypeDef result;
+
   // Tell TMP100 that we want to read from the temperature register
   buf[0] = 0x00;
-
   result = HAL_I2C_Master_Transmit(i2cHandle, TMP100_addr<<1, buf, 1, HAL_MAX_DELAY);
   if ( result == HAL_OK ) {
     // Read 2 bytes from the temperature register
@@ -65,15 +65,12 @@ uint16_t i2c_read_TMP100(I2C_HandleTypeDef* i2cHandle, uint8_t TMP100_addr){
     if ( result != HAL_OK ) {
       printf("Error TMP100 RX\r\n");
     } else {
-
       //Combine the bytes
       val = ((int16_t)buf[0] << 4) | (buf[1] >> 4);
-
       // Convert to 2's complement, since temperature can be negative
       if ( val > 0x7FF ) {
         val |= 0xF000;
       }
-
       // Convert to float temperature value (Celsius)
       float temp_c = val * 0.0625;
 
