933677c11e983b9cc68ac2bb5391ad5f4ecbd231
JochiSt <jochen.steinmann@web.de>
2022-12-15 18:36:32 +0100

    Make has_new_adc_result volatile
    because this can be changed from various positions.

--- firmware/Inc/adc.h
+++ firmware/Inc/adc.h
@@ -34,7 +34,7 @@ extern "C" {
 extern ADC_HandleTypeDef hadc1;
 
 /* USER CODE BEGIN Private defines */
-uint8_t has_new_adc_result;
+volatile uint8_t has_new_adc_result;
 /* USER CODE END Private defines */
 
 void MX_ADC1_Init(void);
