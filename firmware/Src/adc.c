4ed9b56ca54217ec4557b1e0d2334cf4a4bd2823
JochiSt <jochen.steinmann@web.de>
2022-12-15 18:34:41 +0100

    fixed unused variable
    by just printing if there has been an error

--- firmware/Src/adc.c
+++ firmware/Src/adc.c
@@ -21,6 +21,7 @@
 #include "adc.h"
 
 /* USER CODE BEGIN 0 */
+#include <stdio.h>
 #include "utils.h"
 
 /* USER CODE END 0 */
@@ -173,6 +174,9 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
 
 /* USER CODE BEGIN 1 */
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
+  if (hadc->ErrorCode != HAL_OK){
+	  printf("ADC Error\r\n");
+  }
   // raise flag, that we have a new ADC result
   has_new_adc_result ++;
 }
