f4937ca8db9e607cf4da7a1ac07c2a6ab4dd1294
JochiSt <jochen.steinmann@web.de>
2022-12-16 16:18:02 +0100

    started plotting resistances

--- software/testing/test_OFM_stability.py
+++ software/testing/test_OFM_stability.py
@@ -11,12 +11,27 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    repetitions = 2            	# repeat how many times
-    DAC_START = 16
+    repetitions = 100            	# repeat how many times
+
+    dac_steps = np.array([])
+
+    DAC_START = 0
+    DAC_STOP  = 64
+    DAC_STEP  = 2
+
+    dac_steps = np.append(dac_steps,
+                      np.linspace(DAC_START, DAC_STOP, int( (DAC_STOP - DAC_START)/DAC_STEP +1) ))
+
+
+
+    DAC_START = 64
     DAC_STOP  = 1024
     DAC_STEP  = 64
-    dac_steps = np.linspace(DAC_START, DAC_STOP, int( (DAC_STOP - DAC_START)/DAC_STEP +1) )
-    channel = 0                     # which channel to look at
+
+    dac_steps = np.append(dac_steps,
+                          np.linspace(DAC_START, DAC_STOP, int( (DAC_STOP - DAC_START)/DAC_STEP +1) ))
+
+    channel = 1                     # which channel to look at
 
     try:
         # initialise USBtin
@@ -62,16 +77,15 @@ def main():
         ax1.plot( dacs, voltage1 / voltage0, label="voltage", marker=".", linestyle = "-")
         ax1.plot( dacs, current1 / current0, label="curent", marker=".", linestyle = "-")
 
-
         ax2.plot(dacs, voltage0, label="voltage 0", marker=".", linestyle = "-")
         ax2.plot(dacs, voltage1, label="voltage 1", marker=".", linestyle = "-")
         ax2.plot(dacs, current0, label="current 0", marker=".", linestyle = "-")
         ax2.plot(dacs, current1, label="current 1", marker=".", linestyle = "-")
 
-        voltage0 = voltage0 * 3.3/4095
+        voltage0 = voltage0 * 3.3/4095 # 1 LSB = 0,8 mV
         voltage1 = voltage1 * 3.3/4095
 
-        current0 = current0 * 3.3/4095 * 10e-3
+        current0 = current0 * 3.3/4095 * 10e-3 # 1 LSB = 8 uA
         current1 = current1 * 3.3/4095 * 10e-3
 
         ax1.set_title("voltage & current gain")
@@ -84,9 +98,21 @@ def main():
         ax2.set_ylabel("current / voltage / LSB")
         ax2.legend()
 
-        plt.title("Channel %d"%(channel))
         fig.tight_layout()  # otherwise the right y-label is slightly clipped
         plt.show()
+
+        fig, (ax1) = plt.subplots(1, 1)
+        ax1.plot(dacs, voltage0 / current0, label="resistance 0", marker=".", linestyle = "-" )
+        ax1.plot(dacs, voltage1 / current1, label="resistance 1", marker=".", linestyle = "-" )
+        ax1.legend()
+
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        plt.show()
+
+        np.savez("stability_v2_%s_CH%d_%d.npz"%(time.strftime("%Y%m%d_%H%M%S"), channel, repetitions),
+                        voltage0 = voltage0, current0=current0,
+                        voltage1 = voltage1, current1=current1,
+                        dac_steps=dac_steps)
 
     except Exception as e:
         print(e)
