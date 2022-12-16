d94b16ce2f270a51dfc68ea58701166d10175ea7
JochiSt <jochen.steinmann@web.de>
2022-12-16 09:26:29 +0100

    removed unneeded configuration lines

--- software/testing/test_OFM.py
+++ software/testing/test_OFM.py
@@ -19,14 +19,7 @@ def main():
         # initialise OFM
         ofm =  OpenFlowMeter(usbtin = setup.usbtin, boardID=0x1)
 
-        measurement_successful = False
-
-        # configuration
-        channels = [0,1]            # channels, we want to analyse
-        repetitions = 10            # repeat how many times
-
         dac_steps = [0, 32, 512, 1024] #, 64, 96, 128]
-
 
         for dac in dac_steps:
             print(dac)
@@ -43,13 +36,11 @@ def main():
             voltage0 = voltage0 * 3.3/4095
             voltage1 = voltage1 * 3.3/4095
 
-
             current0 = current0 * 3.3/4095 * 10e-3
             current1 = current1 * 3.3/4095 * 10e-3
 
             print("U", voltage0, voltage1)
             print("I", current0, current1)
-
             print("R", voltage0/current0, voltage1/current1)
 
         ofm.setDAC(0, 0)
