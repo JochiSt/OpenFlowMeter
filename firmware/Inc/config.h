388a595727097c382c3e21bdd39582b1fe981c4d
JochiSt <jochen.steinmann@web.de>
2022-12-16 14:23:35 +0100

    created file for handling the configuration values

---
+++ firmware/Inc/config.h
@@ -0,0 +1,5 @@
+const uint16_t SMOO = 15;     // averaging factor
+                              // gives the number of old samples
+                              // SMOO_MAX - SMOO is number of new samples
+                              // VAL = (val * (SMOO_MAX - SMOO) + Previous_value * SMOO) / SMOO_MAX
+const uint16_t SMOO_MAX = 16; // Maximal value of SMOO
