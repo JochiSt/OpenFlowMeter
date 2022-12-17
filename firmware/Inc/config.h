3c021cd79fb3a1c67eeffd4400d410084c2e0ff3
JochiSt <jochen.steinmann@web.de>
2022-12-17 08:57:08 +0100

    added #define to prevent recursive inclusion

--- firmware/Inc/config.h
+++ firmware/Inc/config.h
@@ -1,5 +1,19 @@
-const uint16_t SMOO = 15;     // averaging factor
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const uint8_t SMOO = 15;      // averaging factor
                               // gives the number of old samples
                               // SMOO_MAX - SMOO is number of new samples
                               // VAL = (val * (SMOO_MAX - SMOO) + Previous_value * SMOO) / SMOO_MAX
-const uint16_t SMOO_MAX = 16; // Maximal value of SMOO
+const uint8_t SMOO_MAX = 16;  // Maximal value of SMOO
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CONFIG_H__ */
