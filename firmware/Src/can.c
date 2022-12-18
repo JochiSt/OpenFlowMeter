57f28325cc70eac1740b72d5e7f9009d80695311
JochiSt <jochen.steinmann@web.de>
2022-12-18 17:05:09 +0100

    API BREAKING updated CAN message parsing
    changed CAN message IDs, which are parsed. Included board ID.

--- firmware/Src/can.c
+++ firmware/Src/can.c
@@ -235,15 +235,20 @@ void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan){
 
 void CAN_parse_message(CAN_RxHeaderTypeDef RxHeader, uint8_t *RxData){
   printf("received message with ID: %x\r\n", (uint16_t) RxHeader.StdId);
-  if ((RxHeader.StdId == 0x123)) {
-      // set PWM values
-      uint16_t pwm1 = RxData[0] <<8 | RxData[1];
-      uint16_t pwm2 = RxData[2] <<8 | RxData[3];
-      
-      printf("PWM1 %x PWM2 %x\r\n", pwm1, pwm2);
-      
-      TIM3->CCR1 = pwm1; // set channel 1 max. 1024
-      TIM3->CCR2 = pwm2; // set channel 2 max. 1024  
+
+  // 0x1?0 HANDLE CONFIGURATION
+  if ( RxHeader.StdId == (0x100 | (cfg.board_ID << 4))) {
+
+  // 0x1?1 DAC settings
+  }else if ( RxHeader.StdId == (0x101 | (cfg.board_ID << 4))) {
+    // set PWM values
+    uint16_t pwm1 = RxData[0] <<8 | RxData[1];
+    uint16_t pwm2 = RxData[2] <<8 | RxData[3];
+
+    printf("PWM1 %x PWM2 %x\r\n", pwm1, pwm2);
+
+    TIM3->CCR1 = pwm1; // set channel 1 max. 1024
+    TIM3->CCR2 = pwm2; // set channel 2 max. 1024
   }else{
     printf("message not parsed\r\n");
   }
