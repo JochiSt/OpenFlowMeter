011d82efed0f4f1c6efe9a260310fd51711f93e7
JochiSt <jochen.steinmann@web.de>
2022-12-21 14:13:41 +0100

    moved variable definition to top of file

--- firmware/Src/can.c
+++ firmware/Src/can.c
@@ -32,6 +32,9 @@ uint8_t can_filter_bank = 0;
 /// is there a new message, which we have received?
 uint8_t can_message_received = 0;
 
+CAN_RxHeaderTypeDef   RxHeader;
+uint8_t               RxData[8];
+uint8_t data[8] = {0};        ///< bytes, which are send via the CAN bus
 /* USER CODE END 0 */
 
 CAN_HandleTypeDef hcan;
@@ -213,9 +216,6 @@ void CAN_prepare_filter_mask(uint16_t maskID0, uint16_t canID0, uint16_t maskID1
   }
 }
 
-CAN_RxHeaderTypeDef   RxHeader;
-uint8_t               RxData[8];
-uint8_t data[8] = {0};        ///< bytes, which are send via the CAN bus
 /**
  * CAN RX interrupt callback
  */
