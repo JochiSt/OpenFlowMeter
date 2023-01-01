1357ab4284a312ae1fce996be94bf22cdd319c16
JochiSt <jochen.steinmann@web.de>
2023-01-01 12:02:18 +0100

    updated for loops and changed slightly the calculations

--- firmware/calculations/I_U_resolution.py
+++ firmware/calculations/I_U_resolution.py
@@ -54,6 +54,11 @@ if __name__ == "__main__":
     Ugain=5
     Igain=5
 
+    d_sR = {}
+    d_Ulsb = {}
+    d_Ilsb = {}
+
+    R = PT100.convertPT100_R(40)
 
     for Istim in [
              1e-3,
@@ -61,23 +66,45 @@ if __name__ == "__main__":
             20e-3,
             30e-3,
             ]:
+        for Ugain, Igain in [
+                (1,1),
+                (5,5),
 
-            Ilsb = I2LSB( Istim * Igain )
-            if Ilsb > 4000:
-                Ilsb = I2LSB(Istim)
-                Igain = 1
+                ]:
 
-            Ulsb = U2LSB( 100 * Istim * Ugain)
-            if Ulsb > 4000:
-                Ulsb = U2LSB( 100*Istim )
-                Ugain = 1
+                if Istim not in d_sR.keys():
+                    d_sR[Istim] = np.array([])
+                    d_Ulsb[Istim] = np.array([])
+                    d_Ilsb[Istim] = np.array([])
 
-            U, I, R, sU, sI, sR = resolution(Ulsb, Ilsb, Ugain, Igain)
+                Ilsb = I2LSB( Istim * Igain )
+                if Ilsb > 4000:
+                    Ilsb = I2LSB(Istim)
+                    Igain = 1
+
+                Ulsb = U2LSB( R * Istim * Ugain)
+                if Ulsb > 4000:
+                    Ulsb = U2LSB( R*Istim )
+                    Ugain = 1
+
+                U, I, R, sU, sI, sR = resolution(Ulsb, Ilsb, Ugain, Igain)
+
+                d_sR[Istim] = np.append(d_sR[Istim], sR)
+                d_Ulsb[Istim] = np.append(d_Ulsb[Istim], Ulsb)
+
+                print("%6.4f +- %5.4f V"%(U, sU), end="\t")
+                print("%7.4f +- %5.4f mA"%(I*1000, sI*1000), end="\t")
+                print("%2d - %2d"%(Ugain, Igain))
+                print("%7.4f +- %6.4f Ohm"%(R, sR), end="\t")
+
+                # add 100 to sR, because we are interested in the deltaT to 0°C
+                print("+- %6.4f °C"%(PT100.convertPT100_T(100+sR) ))
+
+                print("%7.4f %% %7.4f %% %7.4f %% %7.4f %%"%(
+                    sU/U*100, sI/I*100, sR/R*100,
+                    PT100.convertPT100_T(100+sR) / PT100.convertPT100_T(R) * 100
+                    ))
+
+                print()
 
 
-            print("%6.4f +- %5.4f V"%(U, sU), end="\t")
-            print("%7.4f +- %5.4f mA"%(I*1000, sI*1000), end="\t")
-            print("%2d - %2d"%(Ugain, Igain), end="\t")
-            print("%7.4f +- %6.4f Ohm"%(R, sR))
-
-            print("%7.4f %% %7.4f %% %7.4f %%"%(sU/U*100, sI/I*100, sR/R*100))
