3c07efb9e2fa089ea0c2846204fda31095bae06b
JochiSt <jochen.steinmann@web.de>
2023-01-04 09:41:38 +0100

    changed to global try except finally structure

--- software/testing/test_calibration.py
+++ software/testing/test_calibration.py
@@ -11,45 +11,57 @@ import time
 
 import numpy as np
 def main():
-    # initialise USBtin
-    cansetup = CANsetup()
-    # initialise DMM
-    dmmsetup = DMMsetup()
-    dmm = dmmsetup.dmm
-
-    # initialise OFM
-    ofm =  OpenFlowMeter(usbtin = cansetup.usbtin, boardID=0x1)
-    measurement_successful = False
-
-    # configuration
-    channel = 0            # channels, we want to analyse
-    repetitions = 10      # repeat how many times
-    dac_steps = np.linspace(0,1024, 100).astype(int)
-
-    # just for testing python
-#    repetitions = 1
-#    dac_steps = [128]
+    dmmsetup = None
+    cansetup = None
 
     try:
-        voltage, current, MMcurrent = CurrentCalibration(ofm=ofm, dmm=dmm, channel=channel, repetitions = repetitions, DACs = dac_steps)
-        measurement_successful = True
+        # initialise USBtin
+        cansetup = CANsetup()
+        # initialise DMM
+        dmmsetup = DMMsetup()
+        dmm = dmmsetup.dmm
+
+        # initialise OFM
+        ofm =  OpenFlowMeter(usbtin = cansetup.usbtin, boardID=0x1)
+        measurement_successful = False
+
+        # configuration
+        channel = 0            # channels, we want to analyse
+        repetitions = 10      # repeat how many times
+        dac_steps = np.linspace(0, 99, 100).astype(int)
+        dac_steps = np.append(dac_steps, np.linspace(100, 1024, 100).astype(int))
+
+        print(dac_steps)
+
+        # just for testing python
+    #    repetitions = 1
+    #    dac_steps = [128]
+
+        try:
+            voltage, current, MMcurrent = CurrentCalibration(ofm=ofm, dmm=dmm, channel=channel, repetitions = repetitions, DACs = dac_steps)
+            measurement_successful = True
+        except Exception as e:
+            print(e)
+            pass
+
+        if not measurement_successful:
+            print("Measurement has not been successuful!")
+            return
+
+        # save recorded data in NPZ files
+        np.savez("calibration_%s_CH%d_%d.npz"%(time.strftime("%Y%m%d_%H%M%S"), channel, repetitions),
+                            voltage0=voltage[0], current0=current[0],
+                            voltage1=voltage[1], current1=current[1],
+                            MMcurrent=MMcurrent, dac_steps=dac_steps)
     except Exception as e:
         print(e)
         pass
-
-    # deinit all SETUPs
-    dmmsetup.deinit()
-    cansetup.deinit()
-
-    if not measurement_successful:
-        print("Measurement has not been successuful!")
-        return
-
-    # save recorded data in NPZ files
-    np.savez("calibration_%s_CH%d_%d.npz"%(time.strftime("%Y%m%d_%H%M%S"), channel, repetitions),
-                        voltage0=voltage[0], current0=current[0],
-                        voltage1=voltage[1], current1=current[1],
-                        MMcurrent=MMcurrent, dac_steps=dac_steps)
+    finally:
+        # deinit all SETUPs
+        if dmmsetup:
+            dmmsetup.deinit()
+        if cansetup:
+            cansetup.deinit()
 
 if __name__ == "__main__":
     main()
