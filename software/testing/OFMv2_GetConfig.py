857723e1a637876d3e6ec8ba49c72ce4c46c6aa5
JochiSt <jochen.steinmann@web.de>
2023-01-05 11:43:41 +0100

    added script, which gets the config from the device and prints it

---
+++ software/testing/OFMv2_GetConfig.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("../")
+from OpenFlowMeter import OpenFlowMeter
+
+from CANsetup import CANsetup
+
+import time
+
+
+def GetConfigFromDevice():
+    setup = None
+    try:
+        print("Getting the configuration from OFM")
+        # initialise USBtin
+        setup = CANsetup()
+
+        # initialise OFM
+        ofm =  OpenFlowMeter(usbtin = setup.usbtin, boardID=0x1)
+        #ofm.saveCofig2EEPROM(default=1)     # save the default config in EEPROM
+
+        ofm.requestConfigFromDevice()
+        time.sleep(5)
+        ofm.config.printout()
+
+    except Exception as e:
+        print(e)
+        pass
+
+    finally:
+        if setup:
+            setup.deinit()
+
+if __name__ == "__main__":
+    GetConfigFromDevice()
+
+
