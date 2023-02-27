470e69f1b66f564a444a64e53e074e8c6736443f
Jochen Steinmann <jochen.steinmann@web.de>
2023-02-27 23:01:53 +0100

    updated filename

--- software/testing/plot_OFMv2_PID.py
+++ software/testing/plot_OFMv2_PID.py
@@ -198,6 +198,6 @@ def plot_OFMv2_PID(filename):
     plt.show()
 
 if __name__ == "__main__":
-    plot_OFMv2_PID('PIDtest_20230227_153203.npz')
+    plot_OFMv2_PID('PIDtest_20230227_161925.npz')
 
 
