e56d75200e0a765549c8840a07b8864086be8a53
Jochen Steinmann <jochen.steinmann@web.de>
2023-03-07 11:56:06 +0100

    change Uout with Uadc and solve to Uin

--- firmware/calculations/offset_amplifier.py
+++ firmware/calculations/offset_amplifier.py
@@ -7,12 +7,12 @@ from sympy import Symbol, symbols, solve, pprint, Eq
 Up, Un = symbols("U_+, U_-")
 
 
-Uout  = Symbol("U_out")
+Uadc  = Symbol("U_adc")
 Ubias = Symbol("U_bias")
 
 R1, R2 = symbols("R_1, R_2")
 
-I_R1R2 = (Uout - Ubias) / (R1 + R2)
+I_R1R2 = (Uadc - Ubias) / (R1 + R2)
 
 Un = I_R1R2 * R2 + Ubias
 
@@ -23,6 +23,6 @@ Uin = symbols("U_in")
 eq1 = eq1.subs(Up, Uin)
 pprint(eq1)
 
-sol = solve(eq1, Uout)[0].factor()
+sol = solve(eq1, Uin)[0].factor()
 pprint(sol)
 
