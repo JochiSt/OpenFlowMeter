1b875682d64b57b8ae3788eef48d4e29cc1f71bb
Jochen Steinmann <jochen.steinmann@web.de>
2023-03-08 13:03:23 +0100

    get and print traces and their properties

--- simulations/CurrentSource/simulate_current_source.py
+++ simulations/CurrentSource/simulate_current_source.py
@@ -24,8 +24,21 @@ def processing_data(raw_filename, log_file, Uset):
     # print simulation properties
     #print(LTR.get_raw_property())
 
-    current_measure = LTR.get_trace("V(current_measure)").get_wave(0)
-    voltage_measure = LTR.get_trace("V(voltage_measure)").get_wave(0)
+    current_setpoint= np.mean(LTR.get_trace("V(current_setpoint)").get_wave(0))
+
+    current_pt100   = np.mean(LTR.get_trace("I(PT100)").get_wave(0))*1000
+
+    current_measure = np.mean(LTR.get_trace("V(current_measure)").get_wave(0))
+    voltage_measure = np.mean(LTR.get_trace("V(voltage_measure)").get_wave(0))
+
+    print(current_setpoint)
+    print(current_pt100)
+    print(current_measure)
+    print(voltage_measure)
+
+    R_PT100 = voltage_measure / (current_measure * 10 / 1000)
+    print("R_PT100: ", R_PT100)
+    print("Temperature", PT100.convertPT100_T(R_PT100))
 
 
 # select spice model
