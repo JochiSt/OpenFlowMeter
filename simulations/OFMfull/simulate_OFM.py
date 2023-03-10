8f873788065c131e402d3c40e41eabc2ab220c47
Jochen Steinmann <jochen.steinmann@web.de>
2023-03-10 09:10:09 +0100

    pretty print for traces and properties

--- simulations/OFMfull/simulate_OFM.py
+++ simulations/OFMfull/simulate_OFM.py
@@ -8,6 +8,8 @@ import sys
 from functools import partial # import partial for tweaking callback
 import numpy as np
 
+import pprint
+
 sys.path.append("../PyLTSpice/")
 from PyLTSpice import SimCommander, LTSteps, RawRead
 
@@ -24,10 +26,14 @@ def processing_data(raw_filename, log_file):
     print("Handling the simulation data of %s, log file %s" % (raw_filename, log_file))
     LTR = RawRead(raw_filename)
     # print all stored traces
-    print(LTR.get_trace_names())
+    print("Traces:")
+    for name in LTR.get_trace_names():
+        print(name)
+    print()
 
     # print simulation properties
-    #print(LTR.get_raw_property())
+    print(LTR.get_raw_property())
+    pprint.pprint(LTR.get_raw_property())
 
     uadc_in = np.mean(LTR.get_trace("V(uadc)").get_wave(0)[:-10])
     iadc_in = np.mean(LTR.get_trace("V(iadc)").get_wave(0)[:-10])
